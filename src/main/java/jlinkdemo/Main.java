fb5ae0a2c41d53cba3863a91d19494a3799dff1b
Steve Perkins <steve@steveperkins.com>
2017-10-10 13:13:46 -0400

    Initial commit.  Basic working JavaFX example.

---
+++ src/main/java/jlinkdemo/Main.java
@@ -0,0 +1,28 @@
+package jlinkdemo;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Write 'Hello World' on console output");
+        btn.setOnAction(event -> System.out.println("Hello World!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+
+}
